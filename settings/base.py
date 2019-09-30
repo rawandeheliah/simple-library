f5b0720a5980e9017d618ada7c2462687746155b
rawandeheliah <rawandeheliah@gmail.com>
2019-09-30 14:06:41 +0300

    deleting pipfile

--- settings/base.py
+++ settings/base.py
@@ -2,8 +2,6 @@ import os
 
 import dj_database_url
 
-import django_heroku
-
 
 def populate_hosts():
     hosts = os.environ.get('ALLOWED_HOSTS')
@@ -120,5 +118,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-django_heroku.settings(locals())
--- settings/development.py
+++ settings/development.py
@@ -1,3 +1,6 @@
 from settings.base import *
 
 DEBUG = False
+
+import django_heroku
+django_heroku.settings(locals())
