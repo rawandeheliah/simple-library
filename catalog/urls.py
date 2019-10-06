d8edf854d8c31af13960b4b2810892f9daeb7ea0
rawandeheliah <rawandeheliah@gmail.com>
2019-10-06 11:36:34 +0300

    writing first view

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+
 from . import views
 
 
 app_name = 'catalog'
 urlpatterns = [
-    path('', views.Index),
-    ]
+    path('', views.IndexView.as_view(), name='index'),
+    path('books/', views.DetailView.as_view(), name='book'),
+    path('authors/', views.DetailView.as_view(), name='author'),
+    path('book/<int:pk>/', views.ResultsView.as_view(), name='bookDetail'),
+    # ex: /polls/5/vote/
+    path('author/<int:pk>/', views.vote, name='authorDetail'),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from .models import Author, Genre, Language, Book, BookInstance
+from django.http import Http404
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 
-# Create your views here.
-class Index:
-    pass
-
+class IndexView(generic.DetailView):
+    template_name = 'catalog/index.html'
+    book_count = Book.objects.count()
+    copies_count = BookInstance.objects.count()
+    copies_available_count = BookInstance.objects.filter(status='Available')
+    Authors_count = Author.objects.count()
+    context_object_name = {"book_count": book_count,
+                           "copies_count": copies_count,
+                           "copies_available_count": copies_available_count,
+                           "Authors_count": Authors_count}
