a8cb46216e772cf286dcdc7baf7cb8bddaba9a34
rawandeheliah <rawandeheliah@gmail.com>
2019-10-16 15:15:32 +0300

    changes

--- settings/development.py
+++ settings/development.py
@@ -4,7 +4,6 @@ import django_heroku
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'catalog.storage_backends.MediaStorage'
 
-ALLOWED_HOSTS = ['*']
 DEBUG = True
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
