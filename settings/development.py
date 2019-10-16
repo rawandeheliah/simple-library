dfac6ea86f7f79672096673930ffe3383e6a1f97
rawandeheliah <rawandeheliah@gmail.com>
2019-10-16 14:25:44 +0300

    changes

--- settings/development.py
+++ settings/development.py
@@ -4,8 +4,8 @@ import django_heroku
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'catalog.storage_backends.MediaStorage'
 
-ALLOWED_HOSTS = ['https://still-tundra-14682.herokuapp.com']
-DEBUG = True
+ALLOWED_HOSTS = ['still-tundra-14682.herokuapp.com']
+DEBUG = False
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 
