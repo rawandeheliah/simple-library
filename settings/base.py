144e703104a710073acd75efe1979ae88b47359c
rawandeheliah <rawandeheliah@gmail.com>
2019-10-16 23:51:18 +0300

    changes

--- settings/base.py
+++ settings/base.py
@@ -142,11 +142,12 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'catalog.storage_backends.MediaStorage'
 STATICFILES_LOCATION = 'static'
 
+STATIC_URL = 'static'
 STATIC_ROOT = 'https://still-tundra-14682.s3.amazonaws.com/static/'
 LOGIN_REDIRECT_URL = '/catalog'
 LOGOUT_REDIRECT_URL = '/catalog'
