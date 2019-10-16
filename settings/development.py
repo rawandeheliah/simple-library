3ebc0f74ea23a7a5f0057061e8d1be28f53054cb
rawandeheliah <rawandeheliah@gmail.com>
2019-10-16 14:16:18 +0300

    changes

--- settings/development.py
+++ settings/development.py
@@ -5,7 +5,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'catalog.storage_backends.MediaStorage'
 
 ALLOWED_HOSTS = ['https://still-tundra-14682.herokuapp.com']
-DEBUG = False
+DEBUG = True
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 
