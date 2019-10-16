76e72c1c6bd7799542fdd1f12cc31c07a84c00e3
rawandeheliah <rawandeheliah@gmail.com>
2019-10-16 15:35:43 +0300

    changes

--- settings/base.py
+++ settings/base.py
@@ -145,7 +145,7 @@ AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'catalog.storage_backends.MediaStorage'
-
+STATICFILES_LOCATION = 'static'
 
 
 LOGIN_REDIRECT_URL = '/catalog'
