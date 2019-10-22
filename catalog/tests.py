5ead0d110c6895589e36260708b1ee4a1df811c4
rawandeheliah <rawandeheliah@gmail.com>
2019-10-22 15:52:37 +0300

    adding fixtures

--- catalog/tests.py
+++ catalog/tests.py
@@ -12,50 +12,23 @@ from .models import (Author, Book, Genre, Language, BookInstance)
 
 
 class MyTests(TestCase):
+    fixtures = ['app_fixtures.json']
 
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
-        self.user = User.objects.create(username='johnsnow')
-        self.user.set_password('12345')
+        self.user = User.objects.get(pk=1)
+        self.user.set_password(self.user.password)
         self.user.save()
         self.client.login(username='johnsnow', password='12345')
-        self.author1 = Author.objects.create(name='Mahmoud Darwish',
-                                             date_of_birth=datetime.date(1941, 3, 31),
-                                             date_of_death=datetime.date(2008, 8, 9),
-                                             nationality='Palestinian',
-                                             place_of_birth='Palestine')
-        self.author1.save()
-        self.author2 = Author.objects.create(name='Ghasan Kanfani',
-                                             date_of_birth=datetime.date(1936, 3, 31),
-                                             date_of_death=datetime.date(1976, 7, 9),
-                                             nationality='Palestinian',
-                                             place_of_birth='Palestine')
-        self.author2.save()
-        self.genre = Genre.objects.create(name='drama')
-        self.genre.save()
-        self.language = Language.objects.create(name='Arabic')
-        self.language.save()
-        self.book1 = Book.objects.create(name='bortqal yafa',
-                                         summary='summary',
-                                         author=self.author2)
-        self.book1.genres.add(self.genre)
-        self.book1.languages.add(self.language)
-        self.book1.save()
-        self.book2 = Book.objects.create(name='book1',
-                                         summary='summary',
-                                         author=self.author1)
-        self.book2.genres.add(self.genre)
-        self.book2.languages.add(self.language)
+        self.author1 = Author.objects.get(pk=1)
+        self.author2 = Author.objects.get(pk=2)
+        self.genre = Genre.objects.get(pk=1)
+        self.language = Language.objects.get(pk=1)
 
-        self.book2.save()
-        self.book_ins = BookInstance.objects.create(status='a',
-                                                    language=self.language,
-                                                    due_back_date=datetime.date.today() + datetime.timedelta(days=1),
-                                                    borrower=self.user,
-                                                    )
-        self.book_ins.book.add(self.book1)
-        self.book_ins.save()
+        self.book1 = Book.objects.get(pk=1)
+        self.book2 = Book.objects.get(pk=2)
+        self.book_ins = BookInstance.objects.get(pk=1)
 
     def test_author_list(self):
         response = self.client.get('/catalog/authors/')
@@ -63,8 +36,7 @@ class MyTests(TestCase):
         self.assertEqual(len(author_list), 2)
 
     def test_author_detali_view(self):
-        obj_id = self.author1.id
-        url = '/catalog/author/{}/'.format(obj_id)
+        url = '/catalog/author/{}/'.format(self.author1.id)
         response = self.client.get(url)
         self.assertEqual(response.context['author'].name, 'Mahmoud Darwish')
 
@@ -74,8 +46,7 @@ class MyTests(TestCase):
         self.assertEqual(len(author_list), 2)
 
     def test_book_detali_view(self):
-        obj_id = self.book2.id
-        url = '/catalog/book/{}/'.format(obj_id)
+        url = '/catalog/book/{}/'.format(self.book2.id)
         response = self.client.get(url)
         self.assertEqual(response.context['book'].summary, 'summary')
 
@@ -114,7 +85,6 @@ class MyTests(TestCase):
 
         response = client.get('/catalog/books/create/')
         self.assertEqual(response.status_code, 200)
-        # worked  self.assertEqual(user.has_perm('catalog.can_publish_author'), True)
 
     def test_form_submitting_author(self):
         form_data = {'name': 'new_book',
